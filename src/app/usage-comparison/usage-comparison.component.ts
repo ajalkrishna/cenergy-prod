3f4996db311ffda66b2a58073aa2d2f56b86301f
ajalkrishna <ajalkrishnav@gmail.com>
2023-03-17 15:10:44 +0530

    added peak consumption tracking section

--- src/app/usage-comparison/usage-comparison.component.ts
+++ src/app/usage-comparison/usage-comparison.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { offset } from '@popperjs/core';
+import { Chart, scales } from 'chart.js';
 
 @Component({
   selector: 'app-usage-comparison',
@@ -8,8 +10,72 @@ import { Component, OnInit } from '@angular/core';
 export class UsageComparisonComponent implements OnInit {
 
   constructor() { }
+  progressBar: any;
+  acr:any=0;
 
   ngOnInit(): void {
+    this.makeChart();
+    this.progressBar = document.querySelector('.progress-bar');
+
+    setInterval(()=>{
+    this.progressBar.style.width='0%';
+    this.acr = Math.ceil(Math.random()*2*100)/100
+    this.progressBar.style.width=String((this.acr/2)*100)+'%';      
+    },3000)
   }
 
+  makeChart() {
+    const comparisonPieChart = new Chart("usageAround", {
+      type: "bar",
+      data: {
+        labels: ['Real-time consumption'],
+        datasets: [
+          {
+            data: [100],
+            label: 'Divisional Average Consumption',
+            backgroundColor: 'darkblue',
+            barPercentage: 0.15,
+
+          },
+          {
+            data: [50],
+            label: 'Personal Consumption',
+            backgroundColor: 'blue',
+            barPercentage: 0.15,
+
+          }
+        ]
+      },
+      options: {
+
+        scales: {
+          x: {
+            stacked: true,
+            grid: {
+              display:false,
+            },
+            border: {
+              display: false
+            },
+            ticks:{
+              color:'black',
+            }
+            
+          },
+          y: {
+            stacked: true,
+          }
+        },
+        plugins:{
+          legend:{
+            position:'bottom',
+            align:'start'
+          }
+        }
+      }
+
+
+    })
+
+  }
 }
